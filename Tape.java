98c406118a4c956c7a93f686e6ecbcf806e992e4
emaridette <embmarmol@gmail.com>
2024-04-08 17:39:11 -0400

    tape class

--- Tape.java
+++ Tape.java
@@ -1,7 +1,79 @@
+import java.util.Stack;
+
 public class Tape {
+    private Stack<String> leftStack = new Stack<>();
+    private Stack<String> rightStack = new Stack<>();
+    private String currentChar;
+    private int oneCount = 0;
     
-    public Tape() {
+    public Tape(String input) {
+        for (int i = input.length() - 1; i >= 0; i--) {
+            rightStack.push(input.substring(i, i + 1));
+        }
+        if (rightStack.empty()) {
+            currentChar = ".";
+        } 
+        else {
+            currentChar = rightStack.pop();
+        }
+    }
+
+    public void left() {
+        rightStack.push(currentChar);
+
+        if (leftStack.empty()) {
+            currentChar = ".";
+        } 
+        else {
+            currentChar = leftStack.pop();
+        }
+    }
+
+    public void right() {
+        leftStack.push(currentChar);
+
+        if (rightStack.empty()) {
+            currentChar = ".";
+        } 
+        else {
+            currentChar = rightStack.pop();
+        }
+    }
+
+    public String read() {
+        return currentChar;
+    } 
+
+    public void write(String newString) {
+        if (currentChar == "1") {
+            oneCount--;
+        }
+
+        if (newString == "1") {
+            oneCount++;
+        }
+
+        currentChar = newString;
+    }
+
+    public String toString() {
+        String toPrint = "";
         
+        while (!leftStack.empty()) {
+            toPrint = leftStack.pop() + " " + toPrint;
+        }
+
+        toPrint = toPrint + " " + currentChar;
+
+        while (!rightStack.empty()) {
+            toPrint = toPrint + " " + rightStack.pop();
+        }
+
+        return toPrint;
+    }
+
+    public int oneCount() {
+        return oneCount;
     }
 
 }
