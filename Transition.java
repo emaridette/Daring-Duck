e0bb1404954f72e6652ca35af390621f9af115bc
anaajesh <anaghaajesh2010@gmail.com>
2024-04-08 14:07:45 -0400

    transition class

--- Transition.java
+++ Transition.java
@@ -1,3 +1,30 @@
 public class Transition {
-    
+    private char inputChar;
+    private char writtenChar;
+    private String direction;
+    private String nextState;
+
+    public Transition(char inputChar, char writtenChar, String move, String nextState) {
+        this.inputChar = inputChar;
+        this.writtenChar = writtenChar;
+        this.direction = move;
+        this.nextState = nextState;
+    }
+
+    public char getInputChar() {
+        return inputChar;
+    }
+
+    public char getWrittenChar() {
+        return writtenChar;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getNextState() {
+        return nextState;
+    }
+
 }
